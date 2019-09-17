3eae49dbf4bcc35424a000fca499a89b6391a33c
sechchr22 <980@holbertonschool.com>
2019-09-17 19:38:57 +0000

    function that prints all integers of a list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,13 +2,3 @@
 def print_list_integer(my_list=[]):
     for item in my_list:
         print("{:d}".format(item))
-
-
-'''
-Revisar que pasa si añado el
-if __name__ == "__main__":
-    import sys
-    fib(int(sys.argv[1]))
-si no estoy mal esto es para que tanto funcione
-como script tanto como en interactivo
-'''
