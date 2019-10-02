8401ad99209914c67eb6dfb16a88dbbcd39b752f
sechchr22 <980@holbertonschool.com>
2019-10-02 01:23:24 +0000

    defining Square class with an atribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Square:
+
+    """
+    Square with its size being private
+
+    Args:
+        size: just the size of the square indexed by user
+
+    Attributes:
+        _Square__size: square size
+    """
+
+    def __init__(self, size):
+        self._Square__size = size
