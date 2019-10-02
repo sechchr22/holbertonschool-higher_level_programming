d7442bae5777fd0416fe4d2f3b44ed4660afb434
sechchr22 <980@holbertonschool.com>
2019-10-02 04:57:57 +0000

    Adding attribute position with its 2 propiertis and alsoadding more to the public instance method print

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -9,16 +9,8 @@ class Square:
     """
 
     def __init__(self, size=0, position=(0, 0)):
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
             self.__size = size
-        if type(position) == tuple and len(position) == 2:
             self.__position = position
-        else:
-            raise TypeError("position must be a tuple of 2 positive integers")
 
     @property
     def size(self):
