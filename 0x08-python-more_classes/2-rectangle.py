3569df4220c8eb85b6b02352c67f2775e6f9eb2a
sechchr22 <980@holbertonschool.com>
2019-10-07 18:59:13 +0000

    Added area and perimetrer instance methods

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+class Rectangle:
+
+    """
+    Rectangle with width and height attributes
+    
+    Args:
+        width: width of the rectangle must be an integer
+        height: height of the rectangle must be an integer
+    Attributes:
+        width
+        height
+    """
+
+    def __init__(self, width=0, height=0):
+        if type(width) != int:
+            raise TypeError('width must be an integer')
+        elif width < 0:
+            raise ValueError('width must be >= 0')
+        if type(height) != int:
+            raise TypeError('height must be an integer')
+        elif height < 0:
+            raise ValueError('height must be >= 0')
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+        To know rectangle´s width
+        Return: rectangle´s width attribute
+        """
+        return(self.__width)
+
+    @width.setter
+    def width(self, value):
+        """
+        To set rectangle´s width
+
+        Args:
+            value: must be an integer 
+         """
+        if type(value) != int:
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        To know rectangle´s height
+
+        Return: rectangle´s height attribute
+        """
+        return(self.__height)
+
+    @height.setter
+    def height(self, value):
+        """
+        To set rectangle´s height
+
+        Args:
+            value: must be an integer
+        """
+        if type(value) != int:
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        return(self.__height * self.__width)
+
+    def perimeter(self):
+        a = 2 * self.__height
+        b = 2 * self.__width
+        return(a + b)
