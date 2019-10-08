95a0d5b94d613c921e362bc5b1d7887b42ac5f1e
sechchr22 <980@holbertonschool.com>
2019-10-08 01:11:58 +0000

    pycodestyle fixed

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -3,7 +3,7 @@ class Rectangle:
 
     """
     Rectangle with width and height attributes
-    
+
     Args:
         width: width of the rectangle must be an integer
         height: height of the rectangle must be an integer
@@ -31,7 +31,7 @@ class Rectangle:
         To set rectangle´s width
 
         Args:
-            value: must be an integer 
+            value: must be an integer
          """
         if type(value) != int:
             raise TypeError('width must be an integer')
@@ -65,7 +65,7 @@ class Rectangle:
     def area(self):
         """
         To calculate rectangle´s area
-        
+
         Returns: rectangle´s area
         """
         return(self.__height * self.__width)
