bbfd8677a2d48fa940e8e32ed7b3894f903d9d5c
sechchr22 <980@holbertonschool.com>
2019-10-17 03:36:17 +0000

    Python Input/Output

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Class Student"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves dictionary representation of instace
+
+        Args:
+            attrs: list choosing what attributes to show
+        """
+        dictionary = self.__dict__
+
+        new_dict = {}
+
+        if attrs is not None:
+            for attribute in attrs:
+                if attribute in dictionary:
+                    new_dict[attribute] = dictionary[attribute]
+            if bool(new_dict) is False:
+                return dictionary
+            else:
+                return new_dict
+        else:
+            return dictionary
