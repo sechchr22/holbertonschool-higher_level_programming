42569be274346a6ddad2863f788f7ff31019cdbf
sechchr22 <980@holbertonschool.com>
2019-10-17 00:31:04 +0000

    Python Input/Output

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -7,10 +7,12 @@ load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 if os.path.exists('add_item.json') is False:
     empty_list = []
+    for i in range(1, len(sys.argv)):
+        empty_list.append(sys.argv[i])
     save_to_json_file(empty_list, 'add_item.json')
 
 else:
     current_list = load_from_json_file('add_item.json')
     for i in range(1, len(sys.argv)):
         current_list.append(sys.argv[i])
-        save_to_json_file(current_list, 'add_item.json')
+    save_to_json_file(current_list, 'add_item.json')
