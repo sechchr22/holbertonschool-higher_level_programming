3b46c29f961cdadd6897a1fdf438fbca2740858b
sechchr22 <980@holbertonschool.com>
2019-10-17 00:20:29 +0000

    Python Input/Output

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -5,7 +5,7 @@ import os.path
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-if os.path.exists('add_item.json') == False:
+if os.path.exists('add_item.json') is False:
     empty_list = []
     save_to_json_file(empty_list, 'add_item.json')
 
