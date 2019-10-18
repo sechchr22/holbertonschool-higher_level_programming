071368e7d95832d2dd1b9dcf9c5378f3ea7afe58
sechchr22 <980@holbertonschool.com>
2019-10-18 04:08:55 +0000

    pascal triangle

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -9,4 +9,15 @@ def pascal_triangle(n):
     Args:
        n: int
     """
-    pass
+    matrix = []
+    vector = []
+    for i in range(n):
+        vector.append(1)
+        vector_copy = vector.copy()
+        matrix.append(vector_copy)
+
+    for i in range(2, len(matrix)):
+        for j in range(1, (len(matrix[i]) - 1)):
+            matrix[i][j] = matrix[i - 1][j - 1] + matrix[i - 1][j]
+
+    return matrix
