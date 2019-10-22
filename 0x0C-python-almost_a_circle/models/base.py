c4a0dd47dc5a0d14bcdf15045a9fb6b628abcbf0
sechchr22 <980@holbertonschool.com>
2019-10-22 07:03:44 +0000

    Python almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,3 +18,28 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) is 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+
+        attributes = ['width', 'height', 'size', 'x', 'y', 'id']
+        list_of_json_objs = []
+
+        for obj in list_objs:
+
+            _dict = {}
+
+            dict_1 = obj.__dict__
+            keys = list(dict_1.keys())
+
+            for key in keys:
+                for attr in attributes:
+                    if key.find(attr) != -1:
+                        _dict[attr] = getattr(obj, attr)
+
+            list_of_json_objs.append(_dict)
+
+        filename = '{}.json'.format(cls.__name__)
+
+        with open('{}'.format(filename), 'a+', encoding='utf-8') as a_file:
+            json.dump(list_of_json_objs, a_file)
--- 0x0C-python-almost_a_circle/tests/rectangle_methods_test.py
+++ 0x0C-python-almost_a_circle/tests/rectangle_methods_test.py
@@ -15,7 +15,7 @@ class RectangleMethodsTestCase(unittest.TestCase):
         self.assertEqual(r.area(), 6)
         self.assertEqual(r_1.area(), 6)
 
-    def test_display(self):
+    def test_display_without_x_y(self):
 
         r = Rectangle(3, 2)
 
@@ -29,6 +29,46 @@ class RectangleMethodsTestCase(unittest.TestCase):
             while(j < width):
                 expected += "#"
                 j += 1
+            expected += "\n"
+            i += 1
+
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            r.display()
+            self.assertEqual(fake_out.getvalue(), expected)
+
+    def test_str(self):
+     
+        r = Rectangle(1, 2, 3, 4, 5)
+        expected = "[Rectangle] (5) 3/4 - 1/2\n"
+        
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            print(r)
+            self.assertEqual(fake_out.getvalue(), expected)
+
+    def test_full_display(self):
+
+        r = Rectangle(3, 2, 2, 1)
+
+        height = 2
+        width = 3
+        x = 2
+        y = 1
+        expected = ""
+
+        for k in range(y):
+            expected += "\n"
+
+        i = 0
+        while (i < height):
+
+            for l in range(x):
+                expected += " "
+
+            j = 0
+            while(j < width):
+                expected += "#"
+                j += 1
+
             expected += "\n"
             i += 1
 
