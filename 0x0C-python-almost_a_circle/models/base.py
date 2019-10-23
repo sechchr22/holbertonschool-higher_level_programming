8793d1afef67df63fc7f7127beab27ffee941153
sechchr22 <980@holbertonschool.com>
2019-10-23 04:10:52 +0000

    fixed

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -105,7 +105,7 @@ class Base:
     def from_json_string(json_string):
         """from json to string"""
 
-        if len(json_string) is 0:
+        if json_string is None or len(json_string) is 0:
             return []
 
         return json.loads(json_string)
