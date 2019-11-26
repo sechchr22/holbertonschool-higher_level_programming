56f79f76031f76eb1865c74012be2c2eac0bb6de
sechchr22 <980@holbertonschool.com>
2019-11-26 03:39:40 +0000

    Adding

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- script that lists all the tables of a database in your MySQL server.
+USE hbtn_0c_0;
+SHOW tables;
--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
 --  script that creates a table called first_table in the current database in your MySQL server
 USE hbtn_0c_0;
-CREATE TABLE IF NOT EXISTS first_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS first_table (id INT,
 name VARCHAR(256));
