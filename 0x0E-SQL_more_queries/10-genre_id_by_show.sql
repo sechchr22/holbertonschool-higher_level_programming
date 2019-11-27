e5611eff5dd2576fc138109743f14c54be3bfd10
sechchr22 <980@holbertonschool.com>
2019-11-27 13:58:33 +0000

    script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 INNER JOIN tv_show_genres
-on tv_show_genres.genre_id = tv_shows.id
+ON tv_show_genres.genre_id = tv_shows.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
