8dd0c45263c20ddde9d6ab0b4d2546839379e129
sechchr22 <980@holbertonschool.com>
2019-12-14 14:27:27 +0000

    Write a script that lists all states from the database hbtn_0e_0_usaW

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,13 +3,19 @@
 module to list all states from database hbtn_0e_0_usa
 """
 import MySQLdb
+from sys import argv
+
+username = argv[1]
+password = argv[2]
+database = argv[3]
+
 
 db = MySQLdb.connect(
     host="localhost",
-    user="root",
-    passwd="gokuaddicte22",
+    user=username,
+    passwd=password,
     port=3306,
-    db="hbtn_0e_0_usa")
+    db=database)
 cur = db.cursor()
 
 cur.execute("SELECT * FROM states ORDER BY states.id")
