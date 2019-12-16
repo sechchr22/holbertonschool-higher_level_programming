22de8f0ffa0b2fa35ab6fdeadc74008a36bb6008
sechchr22 <980@holbertonschool.com>
2019-12-16 00:35:51 +0000

    fixed

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,25 +5,29 @@ module to list all states from database hbtn_0e_0_usa
 import MySQLdb
 from sys import argv
 
-username = argv[1]
-password = argv[2]
-database = argv[3]
-arg = argv[4]
-filtered_arg = arg.split(';')
 
-if len(filtered_arg) is 1:
+if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    arg = argv[4]
+    filtered_arg = arg.split(';')
 
-    db = MySQLdb.connect(
-        host="localhost",
-        user=username,
-        passwd=password,
-        port=3306,
-        db=database)
+    if len(filtered_arg) is 1:
+
+        db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            port=3306,
+            db=database)
 
     cur = db.cursor()
 
     cur.execute(
-        "SELECT DISTINCT cities.name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = '{}'); ORDER BY cities.id".format(
+        "SELECT DISTINCT cities.name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = '{}')".format(
             filtered_arg[0]))
     rows = cur.fetchall()
     print(", ".join([i[0] for i in rows]))
+    cur.close()
+    db.close()
