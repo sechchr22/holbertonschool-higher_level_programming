b8657948a859ae98f3887f7af133ea05b8cad6c9
sechchr22 <980@holbertonschool.com>
2019-12-16 03:47:19 +0000

    script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Prints first state module
+"""
+
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+
+
+if __name__ == '__main__':
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.
+        format(argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    instance = session.query(State.id).filter(State.name == argv[4]).all()
+
+    if not instance:
+        print("Not found")
+    else:
+        print(instance[0][0])
+    session.close()
