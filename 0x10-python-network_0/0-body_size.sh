7ff8a97c9aa5a4a4734cf8406e9b585ff029deec
sechchr22 <980@holbertonschool.com>
2020-01-08 01:06:08 +0000

    script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
+# Script that shows up the body size of the response
 curl -s -o . "$1" -w "%{size_download}"
