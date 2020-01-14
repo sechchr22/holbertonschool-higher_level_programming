c08ac396b5ca37d620f14d044b51434c9ea46ffc
sechchr22 <980@holbertonschool.com>
2020-01-14 00:22:42 +0000

    Learning URLLIB python package

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,7 @@ url = 'https://intranet.hbtn.io/status'
 
 with urllib.request.urlopen(url) as response:
     html = response.read()
-    print('Body response:\n- type: {}\n- content: {}\n- utf8 content: {}'.
+    print('Body response:\n\t- type: \
+{}\n\t- content: {}\n\t- utf8 content: {}'.
           format(type(html), html, str(html.decode('utf-8')))
           )
