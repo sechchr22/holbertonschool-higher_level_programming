6eff2eb1e056e380dd4f91dadb73d2da5e78417b
sechchr22 <980@holbertonschool.com>
2020-01-14 03:09:40 +0000

    Handling status codes with request package

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+Python script that takes in a URL, sends a request
+to the URL and displays the body of the response showing
+status codes
+'''
+
+import requests
+import sys
+
+if __name__ == '__main__':
+
+    r = requests.get(sys.argv[1])
+    status = r.status_code
+
+    if status >= 400:
+        print('Error code: {}'.format(status))
+
+    else:
+        print(r.text)
