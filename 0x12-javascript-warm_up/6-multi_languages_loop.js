efdb5670be59db4fb3f390e041e5e07f304c68b6
sechchr22 <980@holbertonschool.com>
2020-02-06 06:07:17 +0000

    Using array of string and for loop in Javascript

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* Javascript using array of string and a loop */
+const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < strings.length; i++) {
+  console.log(strings[i]);
+}
