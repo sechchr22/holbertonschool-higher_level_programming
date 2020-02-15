7b797c753b2dbf49ab58031bce6883a586fda239
sechchr22 <980@holbertonschool.com>
2020-02-15 02:58:03 +0000

    Working quick sort best alternative to big array

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,48 @@
 const numbers = [];
 let j = 2;
 
+function swap (items, leftIndex, rightIndex) {
+  const temp = items[leftIndex];
+  items[leftIndex] = items[rightIndex];
+  items[rightIndex] = temp;
+}
+
+function partition (items, left, right) {
+  const pivot = items[Math.floor((right + left) / 2)];
+  let i = left;
+  let j = right;
+
+  while (i <= j) {
+    while (items[i] < pivot) {
+      i++;
+    }
+    while (items[j] > pivot) {
+      j--;
+    }
+    if (i <= j) {
+      swap(items, i, j);
+      i++;
+      j--;
+    }
+  }
+  return i;
+}
+
+function quickSort (items, left, right) {
+  let index;
+
+  if (items.length > 1) {
+    index = partition(items, left, right);
+    if (left < index - 1) {
+      quickSort(items, left, index - 1);
+    }
+    if (index < right) {
+      quickSort(items, index, right);
+    }
+  }
+  return items;
+}
+
 if ((process.argv.length === 2) || (process.argv.length === 3)) {
   console.log(0);
 } else {
@@ -10,6 +52,6 @@ if ((process.argv.length === 2) || (process.argv.length === 3)) {
     numbers[i] = Number(process.argv[j]);
     j++;
   }
+  const result = quickSort(numbers, 0, numbers.length - 1);
+  console.log(result[result.length - 2]);
 }
-
-console.log(numbers.sort()[numbers.sort().length - 2]);
