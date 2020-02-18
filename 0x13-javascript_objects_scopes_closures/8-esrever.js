bf601d0eb16592a03e239a4b996e1b1b3e306102
sechchr22 <980@holbertonschool.com>
2020-02-18 21:54:17 +0000

    Adding empty list condition

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 /// reverse list
 exports.esrever = function (list) {
+  if (list.length === 0) {
+    return list;
+  }
   let j = list.length - 1;
   for (let i = 0; i < list.length - 1; i++) {
     if (i === j || i > j) {
